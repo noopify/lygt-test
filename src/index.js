1ca43d3d25cc403063d2d0f4edfd4241df6edc79
Noop <noopify@gmail.com>
2015-08-25 19:32:30 +0200

    Expose sinon & chai

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 //= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 var tasks = require('./tasks');
+var sinon = require('karma-sinon-chai/node_modules/sinon');
+var chai = require('karma-sinon-chai/node_modules/chai');
 
 //= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 //
@@ -17,6 +19,8 @@ module.exports = {
     var t = tasks.use(gulp);
     if (lygt) {
       lygt.merge(lygt.tasks, t);
+      lygt.sinon = sinon;
+      lygt.chai = chai;
     }
     return {
       tasks: t
